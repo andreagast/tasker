c5e0052beb649b2d6c2f123db830651b4a64bac7
Andrea Gastaldello <andreagast@msn.com>
2012-10-06 03:01:11 +0200

    Implemented ContentProvider. MainActivity now extends from ListActivity. Implemented "Add..." dialog.

--- tasker/src/it/gas/tasker/MainActivity.java
+++ tasker/src/it/gas/tasker/MainActivity.java
@@ -1,28 +1,46 @@
 package it.gas.tasker;
 
+import it.gas.tasker.db.TaskerColumns;
+import it.gas.tasker.db.TaskerProvider;
 import android.os.Bundle;
-import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.app.LoaderManager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity implements
+		LoaderManager.LoaderCallbacks<Cursor> {
+	private SimpleCursorAdapter adapter;
+	private AlertDialog dialog;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		getLoaderManager().initLoader(0, null, this);
+		final String[] FROM = { TaskerColumns.TITLE, TaskerColumns.DESCRIPTION };
+		final int[] TO = { android.R.id.text1, android.R.id.text2 };
+		adapter = new SimpleCursorAdapter(this,
+				android.R.layout.simple_list_item_2, null, FROM, TO, 0);
+		setListAdapter(adapter);
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-    @Override
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.menu_add:
@@ -37,18 +55,52 @@ public class MainActivity extends Activity {
 		}
 	}
 
-	public void addPopup(MenuItem mi) {
-    	AlertDialog.Builder b = new AlertDialog.Builder(this);
-    	b.setTitle(R.string.app_name);
-    	b.setCancelable(false);
-    	b.setMessage("WIP (work in progress)");
-    	b.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-			
+	private void addPopup(MenuItem mi) {
+		AlertDialog.Builder b = new AlertDialog.Builder(this);
+		b.setTitle(R.string.app_name);
+		b.setCancelable(true);
+		b.setView(getLayoutInflater().inflate(R.layout.activity_dialog_add,
+				null));
+		b.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
 			public void onClick(DialogInterface dialog, int which) {
 				dialog.dismiss();
 			}
 		});
-    	b.create().show();
-    }
-    
+		b.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+				save();
+			}
+		});
+		dialog = b.create();
+		dialog.show();
+	}
+
+	private void save() {
+		ContentValues values = new ContentValues();
+		TextView v = (TextView) dialog.findViewById(R.id.addTitle);
+		String s = v.getText().toString();
+		values.put(TaskerColumns.TITLE, s);
+		v = (TextView) dialog.findViewById(R.id.addDescription);
+		s = v.getText().toString();
+		values.put(TaskerColumns.DESCRIPTION, s);
+		getContentResolver().insert(TaskerProvider.CONTENT_URI, values);
+		getContentResolver().notifyChange(TaskerProvider.CONTENT_URI, null);
+	}
+
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		return new CursorLoader(this, TaskerProvider.CONTENT_URI, null, null,
+				null, null);
+	}
+
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		adapter.swapCursor(data);
+	}
+
+	public void onLoaderReset(Loader<Cursor> loader) {
+		adapter.swapCursor(null);
+	}
+
 }
--- tasker/src/it/gas/tasker/db/TaskerProvider.java
+++ tasker/src/it/gas/tasker/db/TaskerProvider.java
@@ -2,28 +2,62 @@ package it.gas.tasker.db;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 public class TaskerProvider extends ContentProvider {
+	public static final String AUTHORITY = "it.gas.tasker.provider";
+	public static final String TABLE = "task";
+	public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY
+			+ "/task");
 	private TaskerHelper helper;
+	private static final UriMatcher uriMatcher = new UriMatcher(
+			UriMatcher.NO_MATCH);
 
-	@Override
-	public int delete(Uri arg0, String arg1, String[] arg2) {
-		// TODO Auto-generated method stub
-		return 0;
+	static {
+		uriMatcher.addURI(AUTHORITY, TABLE, 0);
+		uriMatcher.addURI(AUTHORITY, TABLE + "/#", 1);
 	}
 
 	@Override
-	public String getType(Uri arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		switch (uriMatcher.match(uri)) {
+		case 0:
+			SQLiteDatabase db = helper.getWritableDatabase();
+			int del = db.delete("task", selection, selectionArgs);
+			return del;
+		case 1:
+			return 0;
+		default:
+			return 0;
+		}
 	}
 
 	@Override
-	public Uri insert(Uri arg0, ContentValues arg1) {
-		// TODO Auto-generated method stub
-		return null;
+	public String getType(Uri uri) {
+		switch (uriMatcher.match(uri)) {
+		case 0:
+			return "vnd.android.cursor.dir/vnd." + AUTHORITY + "." + TABLE;
+		case 1:
+			return "vnd.android.cursor.item/vnd." + AUTHORITY + "." + TABLE;
+		default:
+			return null;
+		}
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		switch (uriMatcher.match(uri)) {
+		case 0:
+			SQLiteDatabase db = helper.getWritableDatabase();
+			long id = db.insert("task", null, values);
+			return Uri.parse("content://" + AUTHORITY + "/" + TABLE + "/" + id);
+		case 1:
+		default:
+			return null;
+		}
 	}
 
 	@Override
@@ -33,16 +67,32 @@ public class TaskerProvider extends ContentProvider {
 	}
 
 	@Override
-	public Cursor query(Uri arg0, String[] arg1, String arg2, String[] arg3,
-			String arg4) {
-		// TODO Auto-generated method stub
-		return null;
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		SQLiteDatabase db = helper.getWritableDatabase();
+		switch (uriMatcher.match(uri)) {
+		case 0:
+		case 1: // TODO
+			Cursor c = db.query(TABLE, projection, selection, selectionArgs, null,
+					null, sortOrder);
+			c.setNotificationUri(getContext().getContentResolver(), uri);
+			return c;
+		default:
+			return null;
+		}
 	}
 
 	@Override
-	public int update(Uri arg0, ContentValues arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		switch (uriMatcher.match(uri)) {
+		case 0:
+			SQLiteDatabase db = helper.getWritableDatabase();
+			return db.update(TABLE, values, selection, selectionArgs);
+		case 1:
+		default:
+			return 0;
+		}
 	}
 
 }
