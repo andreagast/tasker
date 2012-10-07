97f8ce46492e41d0d123961287583a6cc5dce492
Andrea Gastaldello <andreagast@msn.com>
2012-10-08 00:02:03 +0200

    MainActivity modified to be used in pre-HONEYCOMB android devices.

--- tasker/src/it/gas/tasker/MainActivity.java
+++ tasker/src/it/gas/tasker/MainActivity.java
@@ -2,51 +2,24 @@ package it.gas.tasker;
 
 import it.gas.tasker.db.TaskerColumns;
 import it.gas.tasker.db.TaskerProvider;
-import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.app.AlertDialog;
-import android.app.ListActivity;
 import android.content.ContentValues;
-import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.Loader;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.util.Log;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.app.LoaderManager;
 
-public class MainActivity extends ListActivity implements
-		LoaderManager.LoaderCallbacks<Cursor>, DialogInterface.OnClickListener,
-		AdapterView.OnItemLongClickListener {
-	private SimpleCursorAdapter adapter;
+public class MainActivity extends FragmentActivity implements
+		DialogInterface.OnClickListener {
 	private AlertDialog dialog;
-	private SharedPreferences pref;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		getListView().setOnItemLongClickListener(this);
-
-		pref = PreferenceManager.getDefaultSharedPreferences(this);
-
-		final String[] FROM = { TaskerColumns.TITLE, TaskerColumns.DESCRIPTION };
-		final int[] TO = { android.R.id.text1, android.R.id.text2 };
-		adapter = new SimpleCursorAdapter(this,
-				android.R.layout.simple_list_item_2, null, FROM, TO, 0);
-		setListAdapter(adapter);
-
-		getLoaderManager().initLoader(0, null, this);
 	}
 
 	@Override
@@ -75,6 +48,18 @@ public class MainActivity extends ListActivity implements
 		}
 	}
 
+	public void onClick(DialogInterface dialog, int which) {
+		switch (which) {
+		case DialogInterface.BUTTON_POSITIVE:
+			dialog.dismiss();
+			saveNewTask();
+			break;
+		case DialogInterface.BUTTON_NEGATIVE:
+			dialog.dismiss();
+			break;
+		}
+	}
+
 	private void addPopup(MenuItem mi) {
 		AlertDialog.Builder b = new AlertDialog.Builder(this);
 		// .setTitle(R.string.menu_add);
@@ -97,86 +82,6 @@ public class MainActivity extends ListActivity implements
 		values.put(TaskerColumns.DESCRIPTION, s);
 		getContentResolver().insert(TaskerProvider.CONTENT_URI, values);
 		getContentResolver().notifyChange(TaskerProvider.CONTENT_URI, null);
-	}
-	
-	private void markTaskAsCompleted(long id) {
-		Uri uri = Uri.withAppendedPath(TaskerProvider.CONTENT_URI, "" + id);
-		ContentValues values = new ContentValues();
-		values.put(TaskerColumns.COMPLETE, 1);
-		int row = getContentResolver().update(uri, values, null, null);
-		if (row > 0)
-			Toast.makeText(this, "Set as completed.", Toast.LENGTH_SHORT)
-					.show();
-		else
-			Toast.makeText(this, "Something went wrong :(", Toast.LENGTH_SHORT)
-					.show();
-	}
-
-	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		String sort = TaskerColumns._ID + " DESC";
-		// show completed pref
-		String selection = null;
-		boolean b = pref.getBoolean(PrefActivity.PREF_COMPLETED, false);
-		Log.d(this.getLocalClassName(), "pref_completed: " + b);
-		if (!b)
-			selection = TaskerColumns.COMPLETE + " = 0";
-		// execute
-		String[] projection = { TaskerColumns._ID, TaskerColumns.TITLE,
-				TaskerColumns.DESCRIPTION };
-		return new CursorLoader(this, TaskerProvider.CONTENT_URI, projection,
-				selection, null, sort);
-	}
-
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-		adapter.swapCursor(data);
-	}
-
-	public void onLoaderReset(Loader<Cursor> loader) {
-		adapter.swapCursor(null);
-	}
-
-	public void onClick(DialogInterface dialog, int which) {
-		switch (which) {
-		case DialogInterface.BUTTON_POSITIVE:
-			dialog.dismiss();
-			saveNewTask();
-			break;
-		case DialogInterface.BUTTON_NEGATIVE:
-			dialog.dismiss();
-			break;
-		}
-	}
-
-	public boolean onItemLongClick(AdapterView<?> parent, View view,
-			int position, final long id) {
-		Uri uri = Uri.withAppendedPath(TaskerProvider.CONTENT_URI, "" + id);
-		Cursor c = getContentResolver().query(uri, null, null, null, null);
-		if (!c.moveToFirst()) {
-			Log.w(getLocalClassName(), "Cursor is empty... WTF???");
-			return false;
-		}
-		AlertDialog.Builder build = new AlertDialog.Builder(this);
-		build.setTitle(c.getString(c.getColumnIndex(TaskerColumns.TITLE)));
-		build.setMessage("Set the element as completed?");
-		build.setPositiveButton(R.string.ok,
-				new DialogInterface.OnClickListener() {
-
-					public void onClick(DialogInterface dialog, int which) {
-						markTaskAsCompleted(id);
-						dialog.dismiss();
-					}
-				});
-		build.setNegativeButton(R.string.cancel,
-				new DialogInterface.OnClickListener() {
-
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.dismiss();
-					}
-				});
-		build.create().show();
-		c.close();
-		
-		return true;
 	}
 
 }
---
+++ tasker/src/it/gas/tasker/MainFragment.java
@@ -0,0 +1,127 @@
+package it.gas.tasker;
+
+import it.gas.tasker.db.TaskerColumns;
+import it.gas.tasker.db.TaskerProvider;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+public class MainFragment extends ListFragment implements
+		LoaderManager.LoaderCallbacks<Cursor>,
+		AdapterView.OnItemLongClickListener {
+	private SharedPreferences pref;
+	private SimpleCursorAdapter adapter;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.d("MainFragment", "onCreate fragment");
+
+		pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+		final String[] FROM = { TaskerColumns.TITLE, TaskerColumns.DESCRIPTION };
+		final int[] TO = { android.R.id.text1, android.R.id.text2 };
+		adapter = new SimpleCursorAdapter(getActivity(),
+				android.R.layout.simple_list_item_2, null, FROM, TO, 0);
+		setListAdapter(adapter);
+		getActivity().getSupportLoaderManager().initLoader(0, null, this);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.activity_main_fragment, container);
+		ListView l = (ListView) v.findViewById(android.R.id.list);
+		l.setOnItemLongClickListener(this);
+		return v;
+	}
+
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		String sort = TaskerColumns._ID + " DESC";
+		// show completed pref
+		String selection = null;
+		boolean b = pref.getBoolean(PrefActivity.PREF_COMPLETED, false);
+		Log.d(getActivity().getLocalClassName(), "pref_completed: " + b);
+		if (!b)
+			selection = TaskerColumns.COMPLETE + " = 0";
+		// execute
+		String[] projection = { TaskerColumns._ID, TaskerColumns.TITLE,
+				TaskerColumns.DESCRIPTION };
+		return new CursorLoader(getActivity(), TaskerProvider.CONTENT_URI,
+				projection, selection, null, sort);
+	}
+
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		adapter.swapCursor(data);
+	}
+
+	public void onLoaderReset(Loader<Cursor> loader) {
+		adapter.swapCursor(null);
+	}
+
+	public boolean onItemLongClick(AdapterView<?> parent, View view,
+			int position, final long id) {
+		Uri uri = Uri.withAppendedPath(TaskerProvider.CONTENT_URI, "" + id);
+		Cursor c = getActivity().getContentResolver().query(uri, null, null,
+				null, null);
+		if (!c.moveToFirst()) {
+			Log.w(getActivity().getLocalClassName(),
+					"Cursor is empty... WTF???");
+			return false;
+		}
+		AlertDialog.Builder build = new AlertDialog.Builder(getActivity());
+		build.setTitle(c.getString(c.getColumnIndex(TaskerColumns.TITLE)));
+		build.setMessage("Set the element as completed?");
+		build.setPositiveButton(R.string.ok,
+				new DialogInterface.OnClickListener() {
+
+					public void onClick(DialogInterface dialog, int which) {
+						markTaskAsCompleted(id);
+						dialog.dismiss();
+					}
+				});
+		build.setNegativeButton(R.string.cancel,
+				new DialogInterface.OnClickListener() {
+
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				});
+		build.create().show();
+		c.close();
+
+		return true;
+	}
+
+	private void markTaskAsCompleted(long id) {
+		Uri uri = Uri.withAppendedPath(TaskerProvider.CONTENT_URI, "" + id);
+		ContentValues values = new ContentValues();
+		values.put(TaskerColumns.COMPLETE, 1);
+		int row = getActivity().getContentResolver().update(uri, values, null,
+				null);
+		if (row > 0)
+			Toast.makeText(getActivity(), "Set as completed.",
+					Toast.LENGTH_SHORT).show();
+		else
+			Toast.makeText(getActivity(), "Something went wrong :(",
+					Toast.LENGTH_SHORT).show();
+	}
+
+}
