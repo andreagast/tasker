938bcd48d5b1e38d1e124273a721ceeaa7c1f851
Andrea Gastaldello <andreagast@msn.com>
2012-10-26 20:57:46 +0200

    Now the About show the version of the program. Moved to version 3 (v1.1).

--- tasker/src/it/gas/tasker/AboutActivity.java
+++ tasker/src/it/gas/tasker/AboutActivity.java
@@ -1,9 +1,14 @@
 package it.gas.tasker;
 
 import android.annotation.TargetApi;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 
@@ -16,6 +21,16 @@ public class AboutActivity extends FragmentActivity {
         setContentView(R.layout.activity_about);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
         	getActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView v = (TextView) findViewById(R.id.about_title);
+        try {
+        	//retrieve the package version
+        	PackageManager pacMan = getPackageManager();
+			PackageInfo info = pacMan.getPackageInfo(getPackageName(), 0);
+			v.setText(getString(R.string.app_name) + " v" + info.versionName);
+		} catch (NameNotFoundException e) {
+			Log.w(getLocalClassName(), e.getMessage());
+			v.setText(R.string.app_name);
+		}
     }
     
     @Override
